58baa39337fa8f1d5bafdb70c6d7c0aba7387b2e
azhoubsj <138343605+azhoubsj@users.noreply.github.com>
2023-09-27 17:14:50 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+# 读取档案
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,价格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+# 让使用者输入
 while True:
 	name = input('请输入商品名称:')
 	if name == 'q':
@@ -10,11 +20,12 @@ while True:
 	products.append([name, price])
 print(products)
 
+# 印出购买记录
 for p in products:
 	print(p)
 	print(p[0], '的价格是', p[1], '元')
 
-
+# 写入档案
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,价格\n')
 	for p in products:
